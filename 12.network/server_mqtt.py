cf111349536739cae5920c41676586546fc901cb
junhuanchen <junhuanchen@qq.com>
2018-11-12 16:32:18 +0800

    add mqtt example.

---
+++ 12.network/server_mqtt.py
@@ -0,0 +1,32 @@
+import wifi
+wifi.start()
+
+server_ip = "192.168.30.105"
+client_id = "umqtt_client"
+
+import time
+
+from umqtt.robust import MQTTClient
+
+try:
+
+    c = MQTTClient(client_id, server_ip)
+    c.DEBUG = True
+
+    def sub_cb(topic, msg):
+        print((topic, msg))
+        c.publish(topic, msg)
+
+    c.set_callback(sub_cb)
+
+    if not c.connect(clean_session = False):
+        c.subscribe(b"foo_topic")
+
+    while 1:
+        time.sleep(1)
+        if c.check_msg() is not None:
+            c.wait_msg()
+        else:
+            print('other operator')
+finally:
+    c.disconnect()
