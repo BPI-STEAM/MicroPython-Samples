0db98b452ac78e46090a48af41fe5fcc5fe2bde8
Juwan <junhuanchen@qq.com>
2019-01-22 17:57:20 +0800

    fix uart

--- 02.inputs/uart.py
+++ 02.inputs/uart.py
@@ -4,9 +4,10 @@ from machine import UART
 uart = UART(2)  # UART(2) RX16 TX17
 uart.init(57600, bits=8, parity=None, stop=1)
 
+time.sleep(1)
+uart.write("hello\n")
+
 while True:
     recvlen = uart.any()
     if recvlen > 0:
-        uart.write(uart.read(1))
-    # uart.write("hello\n")
-    # time.sleep(1)
+        uart.write(uart.read(recvlen))
