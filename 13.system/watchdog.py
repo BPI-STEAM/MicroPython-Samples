d6613c15a5e2a0fb14d328986b51fcb627bae039
Juwan <junhuanchen@qq.com>
2019-06-21 18:35:24 +0800

    updated watchdog

--- 13.system/watchdog.py
+++ 13.system/watchdog.py
@@ -1,8 +1,15 @@
 
 import machine
 
-t = machine.WDT(0, 5) # init wdt 0, timeout 5s
+t = machine.WDT(0, 5000) # init wdt 0, timeout 5s
 
 t.feed() # feed it
 
 t.delete() # close it
+
+# t.feed() # feed it
+import time
+while True:
+    print("watchdog...")
+    # t.feed() # feed it
+    time.sleep(1)
